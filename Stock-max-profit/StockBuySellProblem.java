babcaf3d026fab21af7647041e23850cb69d0da9
Mudgil <2267527@cognizant.com>
2023-04-03 12:02:52 +1000

    update--- stock max profit from the given price rates

--- Stock-max-profit/StockBuySellProblem.java
+++ Stock-max-profit/StockBuySellProblem.java
@@ -8,21 +8,29 @@ public class StockBuySellProblem {
 
 	public static void main(String[] args) {
 		//int stock[] = { 100, 180, 260, 310, 40, 535, 695 };
-		int stock[] = { 50, 90, 130, 155, 20, 267, 347 };
+//		int stock[] = { 50, 90, 130, 155, 20, 267, 347 };
+		int stock[] = {14, 12, 10, 15, 99, 65, 21, 90};
 		// int stock[] = { 100, 200, 150, 50, 500 };
 		int profit = 0, buy = 0, sell = 0, pointer = 0;
+		boolean firstFlag=false;
 		List<Integer> profitDiff = new ArrayList<Integer>();
 		label: for (int j = 1; j < stock.length; j++) {
 			if (stock[pointer] < stock[j]) {
 				buy = stock[pointer];
 				profitDiff.add(stock[j] - stock[pointer]);
 				if (j == stock.length - 1) {
+					if(profit == 0) {
+						Optional<Integer> mapMaxprofit = profitDiff.stream().reduce(Integer::max);
+						profit=mapMaxprofit.get();
+						break label;
+					}
 					sell = stock[j];
 					profit += sell - buy;
 					break label;
 				}
+				firstFlag=true;
 			} else {
-				if (j == 1) {
+				if (!firstFlag) {
 					pointer++;
 					continue;
 				}
@@ -33,6 +41,7 @@ public class StockBuySellProblem {
 				profitDiff.removeAll(profitDiff);
 			}
 		}
+		//System.out.println(profitDiff);
 		System.out.println(profit);
 	}
 
